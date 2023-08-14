e0dafba5205c831ac245555a0eb025f84e71e6ac
a-bit-off <abitovtemirkan@gmail.com>
2023-08-14 19:05:23 +0300

    23

---
+++ solution/23/main.go
@@ -0,0 +1,25 @@
+/*
+Удалить i-ый элемент из слайса.
+*/
+package main
+
+import (
+	"fmt"
+	"log"
+)
+
+func main() {
+	slice := []int{1, 2, 3, 4, 5}
+	res, err := delete(slice, 4)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	fmt.Println(res)
+}
+
+func delete(slice []int, i int) ([]int, error) {
+	if i < 0 || i > len(slice)-1 {
+		return nil, fmt.Errorf("Index out of range")
+	}
+	return append(slice[:i], slice[i+1:]...), nil
+}

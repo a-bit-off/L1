a83484a1e46258a134578a1d9391f4f37757e84e
a-bit-off <abitovtemirkan@gmail.com>
2023-08-14 15:32:57 +0300

    14

--- solution/14/main.go
+++ solution/14/main.go
@@ -4,6 +4,41 @@ int, string, bool, channel из переменной типа interface{}.
 */
 package main
 
-func main() {
+import (
+	"fmt"
+	"reflect"
+)
 
+func main() {
+	var variable interface{}
+
+	variable = 21
+	GetType(variable)
+
+	variable = "21"
+	GetType(variable)
+
+	variable = true
+	GetType(variable)
+
+	variable = make(chan int)
+	GetType(variable)
+
+	variable = 45.5
+	GetType(variable)
+}
+
+func GetType(variable interface{}) {
+	switch reflect.TypeOf(variable).Kind() {
+	case reflect.Int:
+		fmt.Println("Тип: int")
+	case reflect.String:
+		fmt.Println("Тип: string")
+	case reflect.Bool:
+		fmt.Println("Тип: bool")
+	case reflect.Chan:
+		fmt.Println("Тип: chan")
+	default:
+		fmt.Println("Тип: неизвестный")
+	}
 }

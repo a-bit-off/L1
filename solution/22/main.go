d5918b67efe8a20f15df6a8b575fa7b311b8fe51
a-bit-off <abitovtemirkan@gmail.com>
2023-08-15 08:42:47 +0300

    22

--- solution/22/main.go
+++ solution/22/main.go
@@ -1,5 +1,28 @@
+/*
+Разработать программу, которая перемножает, делит, складывает,
+вычитает две числовых переменных a,b, значение которых > 2^20.
+*/
+
 package main
 
-func main() {
+import (
+	"fmt"
+	"math/big"
+)
 
+func main() {
+	num1 := big.NewInt(1 << 40)
+	num2 := big.NewInt(1 << 30)
+
+	sum := new(big.Int).Add(num1, num2)
+	sub := new(big.Int).Sub(num1, num2)
+	mul := new(big.Int).Mul(num1, num2)
+	div := new(big.Int).Div(num1, num2)
+
+	fmt.Println("num1:", num1)
+	fmt.Println("num2:", num2)
+	fmt.Println("sum:", sum)
+	fmt.Println("sub:", sub)
+	fmt.Println("mul:", mul)
+	fmt.Println("div:", div)
 }
